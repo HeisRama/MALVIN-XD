c2aeb6da092c663dff891b2ef9b1fc4d8838369f
HeisRama <heisrama39@gmail.com>
2025-08-13 04:21:52 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "",  // add your Semalvin~J39EiAjT#0Cm6AaTi2zQu6x9ldKwKztn8ogboSH1eZjvnGBn-o64ssion Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "263714757857", // put your phone number here 254791718656
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
-  
+  .
   TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
 };
